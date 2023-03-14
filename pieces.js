b1acf7f0f9244b337ceaa705259d86ce615590a8
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-03-14 11:18:02 +0100

    ajout des autres fiches produits

--- pieces.js
+++ pieces.js
@@ -2,35 +2,50 @@
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
-//Création des balises
-const article = pieces[0];
 
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
 
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`; //prixElement.innerText = "Prix: " + article.prix + " €";
 
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 
-//Création des élements pour l'exercice
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+for (let i = 0; i < pieces.length; i++) {
 
-const disponibilitéElement = document.createElement("p");
-disponibilitéElement.innerText = `Disponibilité:${article.disponibilité ? " En stock" : "Rupture de stock"}`;
+    const article = pieces[i];
 
-//Rattchement des balises au DOM
-const sectionFiches = document.querySelector(".fiches");
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // On crée l’élément img.
+    const imageElement = document.createElement("img");
+    // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
+    imageElement.src = article.image;
+    // Idem pour le nom, le prix et la catégorie...
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
 
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-//Ajout des éléments au DOM pour l'exercice
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(disponibilitéElement);
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`; //prixElement.innerText = "Prix: " + article.prix + " €";
+
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+
+    //Création des élements pour l'exercice
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+
+    const disponibilitéElement = document.createElement("p");
+    disponibilitéElement.innerText = `Disponibilité:${article.disponibilité ? " En stock" : "Rupture de stock"}`;
+
+    // On rattache la balise article à la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    // Idem pour le nom, le prix et la catégorie...
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    //Ajout des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(disponibilitéElement);
+    }
